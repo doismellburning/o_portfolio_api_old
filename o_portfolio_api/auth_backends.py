f63aa48076f653a0df6329afa4b095869d01d1ff
George Hickman <george@ghickman.co.uk>
2013-01-26 22:18:09 +0000

    Stop logging all authentications to apease the limiting gods

--- o_portfolio_api/auth_backends.py
+++ o_portfolio_api/auth_backends.py
@@ -10,7 +10,7 @@ logger = logging.getLogger('api')
 class CustomUserModelBackend(ModelBackend):
     def authenticate(self, username=None, password=None):
         """Allow users to log in with their email as well as username."""
-        logger.info('username: {0}'.format(username))
+        # logger.info('username: {0}'.format(username))
         kw = 'email__iexact' if '@' in username else 'username'
         kwargs = {kw: username}
         try:
