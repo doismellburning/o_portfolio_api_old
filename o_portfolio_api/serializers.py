8afa161d871fdfd1fa1308f924fa5a69fabd15db
George Hickman <george@ghickman.co.uk>
2013-01-27 10:45:26 +0000

    Set password on the new user instances

--- o_portfolio_api/serializers.py
+++ o_portfolio_api/serializers.py
@@ -13,8 +13,17 @@ class EntrySerializer(serializers.ModelSerializer):
 
 class RegistrationSerializer(serializers.ModelSerializer):
     class Meta:
-        fields = ('first_name', 'last_name', 'email', 'password')
+        fields = ('first_name', 'last_name', 'username', 'password')
         model = User
+
+    def restore_object(self, attrs, instance=None):
+        user = User(
+            first_name = attrs['first_name'],
+            last_name = attrs['last_name'],
+            username = attrs['username'],
+        )
+        user.set_password(attrs['password'])
+        return user
 
 
 class UserSerializer(serializers.ModelSerializer):
